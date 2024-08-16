91736cfa1d831e15449fc7d4ef4fba92a74ecce2
Rawda197 <jobsfreelancing74@gmail.com>
2024-08-16 20:01:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# codex--your-ai" 
---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import OpenAI from 'openai'
+
+dotenv.config()
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      max_tokens: 3000,
+      temperature: 0,
+    });
+
+    res.status(200).send({
+      bot: response.choices[0].message.content
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
